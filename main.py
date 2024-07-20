c7c3813ca179dd798ee3db2c13dcb75db99f90b1
ilia <boroznovskiyi@gmail.com>
2024-07-20 20:08:45 +0300

    hot fix with naming

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from services.library_service import LisbraryService
+from services.library_service import LibraryService
 from services.cli_service import CliCervice
 from models.repository import LibraryRepository
 from models.models import Library
 
 def main():
-    cli_service = CliCervice(LisbraryService(LibraryRepository(Library())))
+    cli_service = CliCervice(LibraryService(LibraryRepository(Library())))
     cli_service.start()
 
 if __name__ == '__main__':
